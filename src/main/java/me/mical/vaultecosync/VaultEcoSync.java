feaddf62ca1da53389b675deadbdcda33985f61c
Micalhl <micalhl@sina.cn>
2022-08-17 13:20:35 +0800

    update

--- src/main/java/me/mical/vaultecosync/VaultEcoSync.java
+++ src/main/java/me/mical/vaultecosync/VaultEcoSync.java
@@ -5,11 +5,15 @@ import me.mical.vaultecosync.config.ConfigManager;
 import me.mical.vaultecosync.data.DatabaseManager;
 import me.mical.vaultecosync.task.SyncTask;
 import org.serverct.parrot.parrotx.PPlugin;
+import org.serverct.parrot.parrotx.hooks.VaultUtil;
 
 public final class VaultEcoSync extends PPlugin {
 
     @Getter
     private static DatabaseManager databaseManager;
+
+    @Getter
+    private static VaultUtil vaultUtil;
 
     @Override
     protected void preload() {
@@ -19,6 +23,7 @@ public final class VaultEcoSync extends PPlugin {
 
     @Override
     protected void load() {
+        vaultUtil = new VaultUtil(this, true);
         databaseManager = new DatabaseManager(this);
         databaseManager.init();
         new SyncTask(this).run();
