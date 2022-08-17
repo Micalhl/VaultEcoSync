d0c6d46994dae2dbe5ad5201ea7b65413ade6790
Micalhl <micalhl@sina.cn>
2022-08-17 13:56:26 +0800

    update

--- src/main/java/me/mical/vaultecosync/config/ConfigManager.java
+++ src/main/java/me/mical/vaultecosync/config/ConfigManager.java
@@ -11,10 +11,26 @@ import org.serverct.parrot.parrotx.data.autoload.annotations.PAutoloadGroup;
  * @since 2022/8/17 12:31
  */
 @PAutoloadGroup
+@PAutoloadGroup("Database")
 public class ConfigManager extends PConfig {
 
     @PAutoload("DatabaseType")
     public static String DATABASE_TYPE;
+
+    @PAutoload(group = "Database", value = "Host")
+    public static String DATABASE_HOST;
+
+    @PAutoload(group = "Database", value = "Name")
+    public static String DATABASE_NAME;
+
+    @PAutoload(group = "Database", value = "Port")
+    public static int DATABASE_PORT;
+
+    @PAutoload(group = "Database", value = "User")
+    public static String DATABASE_USER;
+
+    @PAutoload(group = "Database", value = "Password")
+    public static String DATABASE_PASSWORD;
 
     private static ConfigManager instance;
 
--- src/main/java/me/mical/vaultecosync/data/impl/DatabaseMysql.java
+++ src/main/java/me/mical/vaultecosync/data/impl/DatabaseMysql.java
@@ -1,6 +1,11 @@
 package me.mical.vaultecosync.data.impl;
 
+import lombok.NonNull;
+import me.mical.vaultecosync.config.ConfigManager;
 import me.mical.vaultecosync.data.Database;
+import org.serverct.parrot.parrotx.PPlugin;
+import org.serverct.parrot.parrotx.utils.HikariCPUtil;
+import org.serverct.parrot.parrotx.utils.i18n.I18n;
 
 /**
  * @author xiaomu
@@ -8,9 +13,25 @@ import me.mical.vaultecosync.data.Database;
  */
 public class DatabaseMysql implements Database {
 
+    private final PPlugin plugin;
+    private final I18n lang;
+
+    public DatabaseMysql(@NonNull PPlugin plugin) {
+        this.plugin = plugin;
+        this.lang = plugin.getLang();
+    }
+
     @Override
     public void init() {
-
+        HikariCPUtil.setSqlConnectionPool(
+                plugin,
+                ConfigManager.DATABASE_HOST,
+                String.valueOf(ConfigManager.DATABASE_PORT),
+                ConfigManager.DATABASE_NAME,
+                ConfigManager.DATABASE_USER,
+                ConfigManager.DATABASE_PASSWORD
+        );
+        // TODO
     }
 
     @Override
