b7964699ac735298ae0918a61d55e122383dae94
Micalhl <micalhl@sina.cn>
2022-08-17 12:17:51 +0800

    init

---
+++ src/main/java/me/mical/vaultecosync/VaultEcoSync.java
@@ -0,0 +1,17 @@
+package me.mical.vaultecosync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VaultEcoSync extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
